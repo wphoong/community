b7a2b6e5a646d4dd6083c388fe1f2c0ccfba8b06
Wilson <wphoong@gmail.com>
2017-05-16 19:18:51 +0000

    removed old migration files

--- db/migrate/20170516161645_remove_comment_id_from_post.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCommentIdFromPost < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :posts, :comment_id, :integer
-  end
-end
--- db/migrate/20170516172435_drop_posts.rb
+++
@@ -1,6 +0,0 @@
-class DropPosts < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :comments
-    
-  end
-end
